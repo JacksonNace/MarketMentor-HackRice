e4112aad9d7f39124ffafdfcd431d7992fb7662d
Jackson Nace <jacksonnace964@gmail.com>
2024-09-20 20:45:29 -0500

    navbarinit

---
+++ frontend/src/components/navbar.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import './navbar.css';
+
+const Navbar = () => {
+  return (
+
+<nav className="navbar">
+  <div className="navbar-left">
+    <a href="/" className="logo">
+      MarketMentor
+    </a>
+  </div>
+  <div className="navbar-center">
+    <ul className="nav-links">
+      <li>
+        <a href="/home">Home</a>
+      </li>
+      <li>
+        <a href="/home1">home1</a>
+      </li>
+      <li>
+        <a href="/home2">home2</a>
+      </li>
+    </ul>
+  </div>
+</nav>
+);
+};
+
+export default Navbar;
